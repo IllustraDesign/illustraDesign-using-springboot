5716c1474b67cd9ef1974c05271946eafec1f445
IllustraDesign <illustradesignstudio25@gmail.com>
2025-04-22 10:57:17 +0530

    Delete src directory

--- src/main/java/com/illustraDesign/ecommerce/store/repository/adminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.illustraDesign.ecommerce.store.repository;
-
-import com.illustraDesign.ecommerce.store.model.admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface adminRepository extends JpaRepository<admin, Long> {
-    admin findByUsername(String username);
-}
